799b056566bf5ebf96f822a62c1182e06f8e7f20
rjgates3 <gates.j.richard@gmail.com>
2019-07-31 11:42:04 -0600

    bomb function completed

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,33 +1,58 @@
 import React from 'react'
-import { threadId } from 'worker_threads';
+// import { threadId } from 'worker_threads';
 
 class Bomb extends React.Component {
-   state= {
-       count:0,
-       message: 'tick'
-   }
+  state= {
+    count:0,
+    message: 'tick'
+  }
+
+  updateCount() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  changeMessage() {
+    if (this.state.count > 8) {
+      this.setState({
+        count: 0,
+        message: 'tick'
+      })
+    }
+    else if (this.state.count === 8) {
+      this.setState({
+        message: 'BOOM!'
+      })
+    }
+    else if(this.state.count %2 === 0) {
+      this.setState({
+        message: 'tick'})
+    }
+    else if(this.state.count %2 !== 0){
+      this.setState({
+        message: 'tock'
+      })
+    }
+  }
+
    componentDidMount() {
     this.interval = setInterval(() => {
       console.log('tick');
-      function changeMessage() {
-          if(this.state.count %2 === 0) {
-            this.setState({
-                message: 'tick'})
-          }
-          else if(this.state.count %2 !== 0){
-              this.setState({
-                  message: 'tock'
-              })
-          }
-      }
+      this.updateCount();
+      this.changeMessage();
     }, 1000)
   }
   componentWillUnmount() {
     clearInterval(this.interval)
   }
+
   render() {
     return (
-      <div>{this.state.message}</div>
+      <div>
+        <p>{this.state.count}</p>
+        <p>{this.state.message}</p>
+      </div>
     )
   }
 }
